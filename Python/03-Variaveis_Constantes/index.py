0b2753fd91d1aafcf473fb8cc19241aecf68a352
vinic-alves <vinicius.alves95@outlook.com>
2022-10-12 09:48:49 -0300

    constantes e variaveis python

---
+++ Python/03-Variaveis_Constantes/index.py
@@ -0,0 +1,24 @@
+age = 27
+name = 'Vinicius'
+
+age2, name2 = 27 , 'Rodrigo' #Podenos salvar duas variáveis na mesma linha
+
+print(f'Meu nome é {name} e eu tenho {age} anos de idade')
+
+# Para declarar uma constante em Python apenas colocamos o nome da constante
+# com letras maíuscula
+
+#Exemplo de constantes
+
+ABS_PATH = '/vinicius/home/ciencia_de_dados/'
+DEBUG = True
+STATES = [
+    'SP',
+    'RJ',
+    'MG',
+]
+AMOUNT = 30.2
+
+# nome de constantes devem utilizar SNAKE_CASE.
+
+

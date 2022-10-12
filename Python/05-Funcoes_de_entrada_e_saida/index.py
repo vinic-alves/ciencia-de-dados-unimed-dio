2591c15a6d6027f1e5a601ab0a3dbdc54b524d83
vinic-alves <vinicius.alves95@outlook.com>
2022-10-12 10:08:58 -0300

    Funções de entrada e saída do Python

---
+++ Python/05-Funcoes_de_entrada_e_saida/index.py
@@ -0,0 +1,17 @@
+# Ler valores com a função e input
+# Exibir valores com a função print
+
+name = input('digite seu nome/n')
+print(name)
+
+# exemplos de print:
+nome = 'Vinicius'
+sobrenome = 'Alves'
+print(nome, sobrenome)
+print(nome, sobrenome, end="...\n")
+print(nome, sobrenome, sep="#")
+#resultados
+
+#Vinicius Alves
+#Vinicius Alves...
+#Vinicius#Alves

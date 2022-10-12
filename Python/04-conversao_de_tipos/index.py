ca34924b6458017923e27c87decc88fb75fe1db0
vinic-alves <vinicius.alves95@outlook.com>
2022-10-12 09:58:33 -0300

    Conversão de tipos em python

---
+++ Python/04-conversao_de_tipos/index.py
@@ -0,0 +1,22 @@
+# exemplos de conversão de tipos em python
+
+preco = 10
+
+print(preco)
+# resultado = 10
+
+#convertendo agora:
+preco = float(preco)
+print(preco)
+# resultado 10.0 virou um número flutuante
+
+# para preservar um número inteiro numa divisão em python, podemos utilizar duas barras //
+
+# exemplo:
+print(preco //2)
+#resultado = 5 -> ou seja um numero inteiro
+
+# para converter para strings usamos o str
+
+# exemplo:
+print(str(preco))

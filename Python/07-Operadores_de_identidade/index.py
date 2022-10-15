25c5cf98041ff298ede1644ca84ad44ec187ccda
vinic-alves <vinicius.alves95@outlook.com>
2022-10-14 22:24:14 -0300

    Operadores de identidade

---
+++ Python/07-Operadores_de_identidade/index.py
@@ -0,0 +1,11 @@
+#operadores de identidade is e is not
+
+#exemplo
+
+curso = "Curso de Python"
+saldo, limite = 200, 200
+nome_curso = curso
+
+print(curso is nome_curso)
+print(curso is not nome_curso)
+
